c6add32d16c2e720f29bc0f2d8c018e12e0df926
geforcesong <georgeguo@outlook.com>
2019-02-12 09:49:25 -0800

    expose jquery

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,20 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     mode: 'development',
+    module: {
+        rules: [
+            {
+                test: require.resolve('jquery'),
+                use: [{
+                    loader: 'expose-loader',
+                    options: 'jQuery'
+                }, {
+                    loader: 'expose-loader',
+                    options: '$'
+                }]
+            }
+        ]
+    },
     plugins: [
         new webpack.ProvidePlugin({
             "$": "jquery",
