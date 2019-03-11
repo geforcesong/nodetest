c767d931531c4ac772dd1836178fe293442216ba
geforcesong <georgeguo@outlook.com>
2019-03-11 10:23:02 -0700

    updated

---
+++ mixes/gitcmd.js
@@ -0,0 +1,34 @@
+const { exec } = require('child_process');
+
+// exec('git add .', (err, stdout, stderr) => {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+
+//     // the *entire* stdout and stderr (buffered)
+//     console.log('Command is executed without error');
+//     console.log(`stdout: ${stdout}`);
+//     console.log(`stderr: ${stderr}`);
+// });
+
+async function executeCommand(command) {
+    return new Promise((resolve, reject) => {
+        exec('git add .', (err, stdout, stderr) => {
+            if (err) {
+                return reject(err);
+            }
+            // the *entire* stdout and stderr (buffered)
+            console.log('Command is executed without error');
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+            return resolve(true);
+        });
+    })
+}
+
+(async _=>{
+    await executeCommand('git add .');
+    await executeCommand('git commit -am "hello"');
+    await executeCommand('git push')
+})();
