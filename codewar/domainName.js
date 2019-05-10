ddb821746caf0df2fa53bf65054fb5fafcf66a4a
geforcesong <georgeguo@outlook.com>
2019-05-10 11:28:12 -0700

    updated

--- codewar/domainName.js
+++ codewar/domainName.js
@@ -1,12 +1,11 @@
-function domainName(url){
-    //your code here
-    const list  = url.match(/[^\/]+(\.[^\/]+)/);
-    const domainFull = list&& list.length?list[0]:'';
-    if(domainFull){
-        return domainFull.split('.').filter(c=>c!='www')[0];
+function domainName(url) {
+    const list = url.match(/[^\/]+(\.[^\/]+)/);
+    const domainFull = list && list.length ? list[0] : '';
+    if (domainFull) {
+        return domainFull.split('.').filter(c => c != 'www')[0];
     }
     return '';
-  }
+}
 
 
   console.log(domainName("http://movoto.com"));
