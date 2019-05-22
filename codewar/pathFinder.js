d0c144877165203be56e5a270b15cd03ad56ad10
geforcesong <georgeguo@outlook.com>
2019-05-22 15:07:25 -0700

    added shortest path

--- codewar/pathFinder.js
+++ codewar/pathFinder.js
@@ -1,16 +1,35 @@
-
+function display(arr){
+    arr.forEach(element => {
+        var c = '';
+        element.forEach(k=>{
+            c+=k.toString().padStart(4, ' ');
+        });
+        console.log(c);
+    });
+    console.log();
+}
 
 function pathFinder(maze) {
     let mazeArr = maze.split('\n').map(c => { return c.trim().split('') });
-    let stack = [];
+    let queue = [];
     if (mazeArr[0][0] === 'W') {
         return false;
     }
-    stack.push({ x:0, y:0 });
-    while (stack.length) {
-        cur = stack.pop();
+    let pathMap = [];
+    mazeArr.forEach(line=>{
+        var mapLine = [];
+        line.forEach(cell=>{
+            mapLine.push(100);
+        });
+        pathMap.push(mapLine);
+    });
+    // display(pathMap);
+    queue.push({ x:0, y:0 });
+    pathMap[0][0] = 0;
+    while (queue.length) {
+        cur = queue.shift();
         if (cur.x === mazeArr.length - 1 && cur.y === mazeArr.length - 1) {
-            return true;
+            return pathMap[mazeArr.length - 1][mazeArr.length - 1];
         }
         mazeArr[cur.x][cur.y] = 'W';
         var down = { x: cur.x + 1, y: cur.y };
@@ -18,19 +37,21 @@ function pathFinder(maze) {
         var left = { x: cur.x, y: cur.y - 1 };
         var right = { x: cur.x, y: cur.y + 1 };
         [down, up, left, right].forEach((c) => {
-            if (mazeArr[c.x] && mazeArr[c.x][c.y] && mazeArr[c.x][c.y] !== 'W') {
-                stack.push(c)
+            if (mazeArr[c.x] && mazeArr[c.x][c.y] && mazeArr[c.x][c.y] !== 'W' && pathMap[c.x][c.y] === 100) {
+                queue.push(c)
+                pathMap[c.x][c.y] = pathMap[cur.x][cur.y] + 1
             }
-        })
+        });
+        // display(pathMap);
     }
     return false;
 }
 
 
 
-console.log(pathFinder(`.W.
-.W.
-W..`));
+// console.log(pathFinder(`.W.
+// .W.
+// W..`));
 
 console.log(pathFinder(
     `......
