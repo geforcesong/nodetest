71ee137f98070ae44634d9131f8c82c4bb81dbdf
geforcesong <georgeguo@outlook.com>
2019-05-24 15:22:43 -0700

    updated version

--- codewar/lengthIntegerArithmetic.js
+++ codewar/lengthIntegerArithmetic.js
@@ -25,21 +25,98 @@ function subtract(minuend, subtrahend) {
     const minuendArr = minuend.toString().split('');
     const subtrahendArr = subtrahend.toString().split('');
     let carry = 0, i = minuendArr.length - 1, j = subtrahendArr.length - 1;
-    const final = [];
+    let final = [];
     while (i >= 0) {
         let a = parseInt(minuendArr[i--]) || 0;
         let b = parseInt(subtrahendArr[j--]) || 0;
         let cur = a + carry - b;
-        if(cur < 0){
+        if (cur < 0) {
             cur = cur + 10;
             carry = -1;
-        } else{
+        } else {
             carry = 0;
         }
         final.unshift(cur);
     }
-    return final.join('');
+    final = final.slice(final.findIndex(n => n));
+    return final.length ? final.join('') : '0';
 }
+
+function multiply(multiplicand, multiplier) {
+    if (!multiplicand || !multiplier || multiplicand == '0' || multiplier === '0') {
+        return '0';
+    }
+    const multiplicandArr = multiplicand.toString().split('');
+    const multiplierArr = multiplier.toString().split('');
+    let j = multiplierArr.length - 1;
+    let final = '0', pos = 0;
+    while (j >= 0) {
+        let cur = [];
+        let carry = 0,  multiplyN = parseInt(multiplierArr[j--]) || 0, i = multiplicandArr.length - 1;
+        while (i >= 0) {
+            let a = multiplyN;
+            let b = parseInt(multiplicandArr[i--]) || 0;
+            let result = a * b + carry;
+            cur.unshift(result % 10);
+            carry = Math.floor(result / 10);
+        }
+        if (carry) {
+            while (carry) {
+                cur.unshift(carry % 10);
+                carry = Math.floor(carry / 10);
+            }
+        }
+        for (let k = 0; k < pos; k++) {
+            cur.push('0');
+        }
+        pos++;
+        final = add(final, cur.join(''));
+    }
+    return final;
+}
+
+function divide(dividend, divisor) {
+    if (!divisor || divisor == 0) {
+        throw new Error('0 can\'t be divided');
+    }
+    if (!dividend || dividend == 0) {
+        return '0';
+    } else if (divisor == '1'){
+        return dividend;
+    } else if(dividend === divisor){
+        return '1';
+    }
+    let tmp  = divisor, times = '1';
+    while(lessThan(tmp, dividend)){
+        times++;
+        tmp = multiply(divisor, times);
+    }
+    if(tmp === dividend){
+        times++;
+    }
+    return String(times-1);
+}
+
+function lessThan(a, b) {
+    if (a.length === b.length) {
+        for (let i = 0; i < a.length; i++) {
+            if (a[i] == b[i]) {
+                continue;
+            } else if (a[i] > b[i]){
+                return false;
+            } else{
+                return true;
+            }
+        }
+        return false;
+    } else {
+        return a.length < b.length;
+    }
+}
+
 
 console.log(add('12345678901234567890', '12345678901234567890') === '24691357802469135780');
 console.log(subtract('24691357802469135780', '12345678901234567890') === '12345678901234567890');
+console.log(multiply('12345678901234567890', '12345678901234567890') === '152415787532388367501905199875019052100');
+
+console.log(divide('24691357802469135780','12345678901234567890') === '2');
