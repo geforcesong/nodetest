00036047d81f25bd90d15837b6a72da8f4191cd7
geforcesong <georgeguo@outlook.com>
2019-06-03 10:57:01 -0700

    google bot detector

---
+++ mixes/googlebot_detect.js
@@ -0,0 +1,29 @@
+const dns = require('dns');
+
+function detectGoogleBot(ip) {
+    return new Promise(function (resolve, reject) {
+        dns.reverse(ip, (error, hosts) => {
+            if (error) {
+                reject(error);
+                return resolve(false);
+            }
+
+            const tld = hosts[0] && hosts[0].split('.').slice(-2, -1)[0];
+            if (tld !== "google" && tld !== "googlebot") {
+                return resolve(false);
+            }
+
+            dns.lookup(hosts[0], (error, address) => {
+                const match = address === ip;
+                return resolve(match);
+            });
+        });
+    });
+}
+
+(async _ => {
+    let c = await detectGoogleBot('66.149.66.1');
+    console.log(`66.149.66.1 ---> ${c}`);
+    c = await detectGoogleBot('66.249.66.1');
+    console.log(`66.249.66.1 ---> ${c}`);    
+})()
