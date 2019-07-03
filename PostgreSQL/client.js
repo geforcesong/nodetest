59f5a2982151377a1182c7e4555e8ed6bfb7c61b
geforcesong <georgeguo@outlook.com>
2019-07-03 16:01:59 -0700

    updated

--- PostgreSQL/client.js
+++ PostgreSQL/client.js
@@ -5,7 +5,7 @@ const connectionStr = 'postgres://george:123456@localhost:5432/Student';
     await client.connect()
     const res = await client.query('SELECT $1::text as message', ['Hello world!'])
     console.log(res.rows[0].message) // Hello world!
-    const studentsRes = await client.query('SELECT * FROM STUDENT');
+    const studentsRes = await client.query('select * FROM STUDENT');
     console.log(studentsRes.rows);
     await client.end()
 })();
