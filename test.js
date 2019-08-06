40ffe9f97a2b77bb5613fce89a06499bb072632f
geforcesong <georgeguo@outlook.com>
2019-08-06 14:10:27 -0700

    u

--- test.js
+++ test.js
@@ -1,15 +1,3 @@
-class A{}
-
-async function test(){
-    return new Promise((resolve)=>{
-        return null;
-    });
-}
-
-// (async _=>{
-    
-// })()
-
 test().then(c=>{
     c.then(d=>{
         console.log(d);
