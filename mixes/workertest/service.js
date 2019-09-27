e58deae99899c050671a54bd320bc242f8f3f92a
geforcesong <georgeguo@outlook.com>
2019-09-27 14:21:11 -0700

    test

--- mixes/workertest/service.js
+++ mixes/workertest/service.js
@@ -2,7 +2,13 @@ const { workerData, parentPort } = require('worker_threads')
 
 // You can do any heavy stuff here, in a synchronous way
 // without blocking the "main thread"
+var m=10;
+for(var i=0;i<50000;i++){
+    m+=i;
+}
+
 parentPort.postMessage( 'hello:'+ workerData )
 parentPort.postMessage({ hello: workerData })
 parentPort.postMessage(100)
+parentPort.postMessage('Total:' + m);
 // throw new Error('not a num')
--- test.js
+++ test.js
@@ -1,7 +1,29 @@
-function A(){
-    console.log(this === global);
-    console.log(this instanceof A);
-    console.log(this);
+
+let Q = [];
+
+function asyncCall(name){
+    let task = new Promise((resolve, reject)=>{
+        Q.push({resolve: resolve.bind(this), reject:reject.bind(this)});
+    });
+    task.then(()=>{
+        console.log(`Task ${name} is done`);
+    }).catch(()=>{
+        console.log(`Task ${name} is canceled.`);
+    });
+    return task;
 }
 
-A()
+asyncCall('task1');
+asyncCall('task2');
+
+setTimeout(()=>{
+    console.log('Now start tasks');
+    while(Q.length){
+        let c = Q.pop();
+        if(Q.length %2==0){
+            c['resolve']();
+        } else{
+            c['reject']();
+        }
+    }
+}, 5000)
