c4d53c51fcae0bdd36e79704cc9f770c6e40efa4
geforcesong <georgeguo@outlook.com>
2020-03-26 13:11:44 +0800

    udpated

--- express-site/public/javascripts/remote3.js
+++
@@ -1,9 +0,0 @@
-document.cookie = "remote_v=3";
-
-setTimeout(() => {
-    console.log();
-    console.log('Read cookie from remote:');
-    var cookie = document.cookie;
-    console.log(cookie);
-    console.log('Read cookie from remote Finished!!');
-}, 5000);
