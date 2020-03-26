ed6ced5b7f441956d90c4b6881363077e0f422d1
geforcesong <georgeguo@outlook.com>
2020-03-26 11:08:45 +0800

    updated tested cookies

--- express-site/public/javascripts/cookietest.js
+++ express-site/public/javascripts/cookietest.js
@@ -1,1 +1,10 @@
 document.cookie = "local_v=1";
+
+
+setTimeout(() => {
+    console.log();
+    console.log('Read cookie from local:');
+    var cookie = document.cookie;
+    console.log(cookie);
+    console.log('Read cookie from local Finished!!');
+}, 3000);
--- express-site/public/javascripts/remotecookie.js
+++ express-site/public/javascripts/remotecookie.js
@@ -1,1 +1,9 @@
 document.cookie = "remote_v=1";
+
+setTimeout(() => {
+    console.log();
+    console.log('Read cookie from remote:');
+    var cookie = document.cookie;
+    console.log(cookie);
+    console.log('Read cookie from remote Finished!!');
+}, 3000);
